c34eabbdc854e64261a537b1775cbef79f0dcad7
Roberto Ramirez <robert.624@hotmail.com>
2023-03-10 11:54:52 -0500

    desktop design finished

--- src/components/TotalizerArea/TotalizerArea.jsx
+++ src/components/TotalizerArea/TotalizerArea.jsx
@@ -4,19 +4,21 @@ import './index.scss'
 const TotalizerArea = ({tipAmount, total, resetFunction}) => {
     return (
         <div className="totalizers-area">
-            <div className="tip-amount">
-                <p>
-                    Tip Amount <span>/ person</span>
-                </p>
-                <span className="amount-number">${tipAmount}</span>
+            <div className="values">
+                <div className="tip-amount">
+                    <p>
+                        Tip Amount <span>/ person</span>
+                    </p>
+                    <span className="amount-number">${tipAmount}</span>
+                </div>
+                <div className="total">
+                    <p>
+                        Total <span>/ person</span>
+                    </p>
+                    <span className="amount-number">${total}</span>
+                </div>
             </div>
-            <div className="total">
-                <p>
-                    Total <span>/ person</span>
-                </p>
-                <span className="amount-number">${total}</span>
-            </div>
-            <button onClick={resetFunction} className="reset">Reset</button>
+            <button onClick={resetFunction} disabled={true} className="reset">Reset</button>
         </div>
     );
 };
